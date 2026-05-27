e2be9e9e073d6fd59832ba5c7972800a3242fdc3
‘darcy’ <darcy.mears@yahoo.com>
2026-05-27 17:53:19 +0100

    The correct card number can be found and its a win

--- SnapGame/src/main/java/org/example/Snap.java
+++ SnapGame/src/main/java/org/example/Snap.java
@@ -9,20 +9,30 @@ public class Snap extends CardGame {
         //The game continues (a loop?) if two cards != same
         //when 2 cards = the same, the game ends.
         System.out.println("Press Enter to Start and draw a card");
+        Card previousCard = null;
         while(true){
 
             Scanner scanner = new Scanner(System.in);
 
             scanner.nextLine();
-            shuffleDeck();
+
             Card dealtCard = dealCard();
             System.out.println(dealtCard);
 
-            System.out.println();
+            if (previousCard != null) {
 
-            if(dealtCard.getNumValue() == deckOfCards.get(deckOfCards.size() -2).getNumValue()){
-                System.out.println("You won. Snap!");
+                if (dealtCard.getNumValue() == previousCard.getNumValue()) {
+                    System.out.println("You won. SNAP!");
+                    break;
+                }
+
+                if (dealtCard.getSymbol() == previousCard.getSymbol()) {
+                    System.out.println("You won. SNAP!");
+                    break;
+                }
             }
+
+            previousCard = dealtCard;
         }
 
 
