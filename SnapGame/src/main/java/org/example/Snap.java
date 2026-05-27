1cfcc5f3cc8c6b8cc3d033f93702c8f4db858291
‘darcy’ <darcy.mears@yahoo.com>
2026-05-27 10:54:55 +0100

    Added players class and pseudo coded the Snap class

--- SnapGame/src/main/java/org/example/Snap.java
+++ SnapGame/src/main/java/org/example/Snap.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Snap extends CardGame {
+    public Snap() {
+        System.out.println("Press Enter to Start");
+        //When the user presses enter the game starts
+        //Each time enter is pressed a new card is dealt
+        //The game continues (a loop?) if two cards != same
+        //when 2 cards = the same, the game ends.
+    }
+
 }
